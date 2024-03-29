d1f1565b9540da86126e46f271b335e9d32cefe6
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-29 11:49:23 +0800

    feat: updated sub text and moved about to separate component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { FaGithub } from "react-icons/fa";
 import { TiSocialLinkedin } from "react-icons/ti";
+import About from "~/components/About";
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,6 +19,9 @@ export default function Index() {
       <h2 className="text-3xl text-white mt-4">
         Junior Web Developer
       </h2>
+      <div className="flex items-center justify-center bg-white shadow-md mt-8 rounded-lg w-[750px] mx-auto">
+        <p className="text-sky2 text-xl m-6">A passionate and enthusiastic learner who loves to build and design. I am well versed with the MERN stack, and am currently learning Rust and Typescript.</p>
+      </div>
       <div className="flex justify-center items-center">
         <FaGithub onClick={() => window.open('https://github.com/zakaryjs', '_blank')} className="text-5xl mt-8 icon shadow-md cursor-pointer"/>
         <TiSocialLinkedin onClick={() => window.open('https://www.linkedin.com/in/zakary-sutherland/', '_blank')} className="text-5xl mt-8 ml-4 icon shadow-md cursor-pointer"/>
@@ -25,9 +29,7 @@ export default function Index() {
       <button onClick={() => window.open('https://zakarysutherland.hashnode.dev/', '_blank')} className="flex items-center justify-center bg-sky2 shadow-md mt-8 rounded-lg w-3/6 h-10 mx-auto">
         <h3 className="text-2xl text-white">View My Blog</h3>
       </button>
-      {/* <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
-        <h1 className="text-3xl text-sky2">About</h1>
-      </div> */}
+      <About />
     </div>
   );
 }
