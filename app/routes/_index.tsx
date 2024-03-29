6ef68f0a535dbda0dd13db75cf4bd183eaebc030
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-29 09:12:47 +0800

    feat: added github and linkedin icons to main header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { FaGithub } from "react-icons/fa";
+import { TiSocialLinkedin } from "react-icons/ti";
 
 export const meta: MetaFunction = () => {
   return [
@@ -16,6 +18,10 @@ export default function Index() {
       <h2 className="text-3xl text-white mt-4">
         Junior Web Developer
       </h2>
+      <div className="flex justify-center items-center">
+        <FaGithub className="text-5xl mt-8 icon shadow-md"/>
+        <TiSocialLinkedin className="text-5xl mt-8 ml-4 icon shadow-md"/>
+      </div>
       <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
         <h1 className="text-3xl text-sky2">About</h1>
       </div>
