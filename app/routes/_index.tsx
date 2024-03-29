303003010465878bce82af1bddd5777df286b0af
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-29 09:45:13 +0800

    feat: made github and linkedin buttons clickable, added blog button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <div className="">
       <h1 className="text-5xl text-white">
         Zakary Sutherland
       </h1>
@@ -19,12 +19,15 @@ export default function Index() {
         Junior Web Developer
       </h2>
       <div className="flex justify-center items-center">
-        <FaGithub className="text-5xl mt-8 icon shadow-md"/>
-        <TiSocialLinkedin className="text-5xl mt-8 ml-4 icon shadow-md"/>
+        <FaGithub onClick={() => window.open('https://github.com/zakaryjs', '_blank')} className="text-5xl mt-8 icon shadow-md cursor-pointer"/>
+        <TiSocialLinkedin onClick={() => window.open('https://www.linkedin.com/in/zakary-sutherland/', '_blank')} className="text-5xl mt-8 ml-4 icon shadow-md cursor-pointer"/>
       </div>
-      <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
+      <button onClick={() => window.open('https://zakarysutherland.hashnode.dev/', '_blank')} className="flex items-center justify-center bg-sky2 shadow-md mt-8 rounded-lg w-3/6 h-10 mx-auto">
+        <h3 className="text-2xl text-white">View My Blog</h3>
+      </button>
+      {/* <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
         <h1 className="text-3xl text-sky2">About</h1>
-      </div>
+      </div> */}
     </div>
   );
 }
