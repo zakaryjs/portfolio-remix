bbc894655811e3c77a0d2cc9c4d3a4ac24215206
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-29 20:33:22 +0800

    feat: added text to about page

--- app/routes/About.tsx
+++ app/routes/About.tsx
@@ -1,16 +1,24 @@
 import Navbar from "~/components/Navbar"
 
 
-
 export default function About() {
     return (
-        <div>
+        <div className="">
             <Navbar />
-            <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
+            <h1 className="text-5xl text-white">
+                About
+            </h1>
+            {/* <div className="flex items-center justify-center bg-white shadow-md mt-20 rounded-lg h-10 border border-solid border-black/5">
                 <h1 className="text-3xl text-sky2">About</h1>
-            </div>
+            </div> */}
             <div className="flex items-center justify-center bg-white shadow-md mt-8 rounded-lg w-[750px] mx-auto">
                     <p className="text-sky2 text-xl m-6">Hi! My name is Zakary. I&apos;m a tech enthusiast, gamer, and keen learner. I have always had a love for all things tech, and have had an interest in programming since a very young age.</p>
+            </div>
+            <div className="flex items-center justify-center bg-white shadow-md mt-8 rounded-lg w-[750px] mx-auto">
+                <p className="text-sky2 text-xl m-6">My first ever experience with programming was in 2013 with Code.org. I then continued to experiment with various resources throughout my school years and by the time I graduated in 2022, I knew I wanted to pursue a career in tech.</p>
+            </div>
+            <div className="flex items-center justify-center bg-white shadow-md mt-8 rounded-lg w-[750px] mx-auto">
+                <p className="text-sky2 text-xl m-6">When I&apos;m not programming, I am either spending time with family and friends, enjoying video games and anime, and thinking about future project ideas.</p>
             </div>
         </div>
     )
